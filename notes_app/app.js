d39fb13e9d1be854e3e285bcfadca346a2293d4f
vaishnavi-shinde12 <vaishushinde312@gmail.com>
2020-07-12 22:09:04 +0530

    cmd line argument

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,30 +1,16 @@
 /* creating a new file */
-const fs = require("fs");
-fs.writeFileSync('notes.txt', 'My name is Vaishnavi,')        // to create a file with content
-fs.appendFileSync('notes.txt', ' I am learning nodeJS !')    // to append (add) extra content to file
+// const fs = require("fs");
+// fs.writeFileSync('notes.txt', 'My name is Vaishnavi,')        // to create a file with content
+// fs.appendFileSync('notes.txt', ' I am learning nodeJS !')    // to append (add) extra content to file
 
-/* Importing our own files */ 
-// const name = require('./utils.js')    // import our file utils
-// console.log(name)                     // use propertys of utils
-   
-// const add = require('./utils.js')
-// const sum = add(4, -2)
-// console.log(sum);
+/* Command line Argument */
+// console.log(process.argv[1])          // here we use the array to show particular line of array
 
-const getNotes = require('./notes.js')   // importing another our own function
-const msg = getNotes()
-console.log(msg)
-
-/* npm module modules */
-const validator = require('validator')
-
-console.log(validator.isEmail('vaishups12@gmail.com'))   // validating email using validator
-console.log(validator.isURL('https://google.com'))          // validating URL using validator  
-
-/* printing in color */
-const chalk = require('chalk')
-const greetingmsg = chalk.cyan('Vaishnavi')
-console.log(greetingmsg)                                                     // changing text color
-console.log(chalk.red('Hello') + chalk.white(' World') + chalk.green(' !'))  // using diff color to diff word
-console.log(chalk.cyan.bgRed.bold.underline('Heyy... My name is vaishu'))    // providing text & bg color, bold, underline
-console.log(chalk.cyan.bgRed.bold.underline.inverse('Heyy... My name is vaishu'))  // using inverse - it will interchange the text and bg color
+const command = process.argv[2]
+console.log(process.argv)
+if(command === 'add') {
+    console.log('Adding note')
+}
+else if(command === 'remove') {
+    console.log('Removing note')
+}
