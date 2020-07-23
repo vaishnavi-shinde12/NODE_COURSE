0515aba627f6c3caad37895357f6f7deeb72d4b5
vaishnavi-shinde12 <vaishushinde312@gmail.com>
2020-07-24 00:18:54 +0530

    basics

--- notes_app/filesystem.js
+++ notes_app/filesystem.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.writeFile('./hello.txt','I am Vaishnavi Paresh Shinde', function(err) {
+fs.writeFile('./hello.txt'," I am miss Vaishnavi Paresh Shinde ", function(err) {
     if(!err) {
         fs.readFile('./hello.txt', function(err, data) {
             if(!err) {
